58883751e3d9503263d868fe22c6fa830a7a00a3
Aboubaker IdHamide <aboubakeridbsn@gmail.com>
2026-05-11 14:26:17 +0100

    init

---
+++ src/main/java/net/idhamide/aboubaker/ExamenJeeApplication.java
@@ -0,0 +1,13 @@
+package net.idhamide.aboubaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenJeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamenJeeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/idhamide/aboubaker/ExamenJeeApplicationTests.java
@@ -0,0 +1,13 @@
+package net.idhamide.aboubaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenJeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
