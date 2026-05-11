f0c0733d245c9b21cf57ee7a65b4de6fb3018036
Aboubaker IdHamide <aboubakeridbsn@gmail.com>
2026-05-11 16:41:49 +0100

    Added Contollers placeholders

---
+++ src/main/java/net/idhamide/aboubaker/controller/VehicleController.java
@@ -0,0 +1,10 @@
+package net.idhamide.aboubaker.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/vehicles")
+public class VehicleController {
+
+}
